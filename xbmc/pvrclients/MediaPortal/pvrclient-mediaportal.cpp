8a4774a0c08c565ca7ec08b876e21a0677b451e3
Marcel Groothuis <margro.xbmc@gmail.com>
2012-02-04 22:47:35 +0100

    MediaPortal PVR addon: work in progress: scan the MediaPortal thumbnails dir for TV and Radio logo's (Vista/7 localhost only at the moment)

--- xbmc/pvrclients/MediaPortal/pvrclient-mediaportal.cpp
+++ xbmc/pvrclients/MediaPortal/pvrclient-mediaportal.cpp
@@ -33,6 +33,7 @@
 #include "SingleLock.h"
 #include "lib/tinyxml/tinyxml.h"
 #include "lib/tsreader/TSReader.h"
+#include "FileUtils.h"
 
 using namespace std;
 using namespace ADDON;
@@ -572,6 +573,7 @@ PVR_ERROR cPVRClientMediaPortal::GetChannels(PVR_HANDLE handle, bool bRadio)
   int             code;
   PVR_CHANNEL     tag;
   CStdString      stream;
+  bool            bCheckForThumbs = false;
 
   if (!IsUp())
     return PVR_ERROR_SERVER_ERROR;
@@ -612,6 +614,20 @@ PVR_ERROR cPVRClientMediaPortal::GetChannels(PVR_HANDLE handle, bool bRadio)
   if( !SendCommand2(command.c_str(), code, lines) )
     return PVR_ERROR_SERVER_ERROR;
 
+#ifdef TARGET_WINDOWS
+  /* Check if we can find the MediaPortal channel logo folders on this machine */
+  std::string strIconName;
+  std::string strThumbPath = "C:\\ProgramData\\Team MediaPortal\\MediaPortal\\Thumbs\\";
+  bCheckForThumbs = OS::CFile::Exists("C:\\ProgramData\\Team MediaPortal\\MediaPortal\\Thumbs\\");
+  if (bCheckForThumbs)
+  {
+    if (bRadio)
+      strThumbPath += "Radio\\";
+    else
+      strThumbPath += "TV\\logos\\";
+  }
+#endif
+
   memset(&tag, 0, sizeof(PVR_CHANNEL));
 
   for (vector<string>::iterator it = lines.begin(); it < lines.end(); it++)
@@ -635,7 +651,22 @@ PVR_ERROR cPVRClientMediaPortal::GetChannels(PVR_HANDLE handle, bool bRadio)
       tag.iUniqueId = channel.UID();
       tag.iChannelNumber = g_iTVServerXBMCBuild >= 102 ? channel.ExternalID() : channel.UID();
       tag.strChannelName = channel.Name();
+#ifdef TARGET_WINDOWS
+      if (bCheckForThumbs)
+      {
+        strIconName = strThumbPath + ToThumbFileName(channel.Name()) + ".png";
+        if ( OS::CFile::Exists(strIconName) )
+        {
+          tag.strIconPath = strIconName.c_str();
+        }
+        else
+        {
+          tag.strIconPath = "";
+        }
+      }
+#else
       tag.strIconPath = "";
+#endif
       tag.iEncryptionSystem = channel.Encrypted();
       tag.bIsRadio = bRadio; //TODO:(channel.Vpid() == 0) && (channel.Apid(0) != 0) ? true : false;
       tag.bIsHidden = false;
